f0adf71c5e22825b1c0b67e100902ec6191dc323
kavitha354 <kavithasekar250403@gmail.com>
2024-11-27 21:02:28 +0530

    Add files via upload

---
+++ e-cart/mens/mens.jsx
@@ -0,0 +1,42 @@
+import React, { useState, useEffect} from 'react';
+import './mens.css'
+import { FaStar, FaRupeeSign } from 'react-icons/fa';
+import productData from '../../productdetails';
+
+
+export const MensItems=()=>{
+    const [productList, setProductList] = useState([]);
+   
+    useEffect(() => {
+      const offerProducts = productData.filter(item => item.category === 'men');
+      console.log(offerProducts)
+      setProductList(offerProducts);
+    }, []);
+  
+    return(
+      <div>
+        <div className="head">
+        
+        <p>Mens Collections...!  </p>
+        
+        </div>
+      <div className="product">
+        {productList.map((item,index) => (
+          <div className='cards' key={index}>
+            <img src={item.image} alt={item.name} />
+            <p className='Pname'>{item.name}</p>
+            <span className='rating'>{item.ratings}<FaStar /><p> Ratings</p></span>
+            <div className='Pr_price'>
+              <p><span><FaRupeeSign/></span>{item.offerprice}</p>
+             
+              <s><span><FaRupeeSign/></span>{item.orginalPrize}</s>
+            </div>
+            <div className="cart">
+              <button className='cart_btn' >Add to cart</button>
+            </div>
+          </div>
+        ))}
+      </div>
+    </div>
+    )
+  }
